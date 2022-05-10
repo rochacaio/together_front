64b0d95891e2518304356d9678fac544e13e5b51
Caio Rocha <caiorocha@deltaglobal.com.br>
2022-05-10 12:14:25 -0300

    adiciona duas primeiras views

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import {Button} from "./components/Button";
+import { Route,BrowserRouter } from "react-router-dom";
+import {Home} from "./pages/Home";
+import {NewRoom} from "./pages/NewRoom";
+//import {NewRoom} from "./pages/NewRoom";
 
 function App() {
   return (
-      <div>
-      <Button/>
-      <Button/>
-      <Button/>
-      </div>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </BrowserRouter>
   );
 }
 
